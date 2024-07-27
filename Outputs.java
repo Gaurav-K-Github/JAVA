5f76ac0455def971c6057cb66d6a8d710596200c
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-07-27 08:17:27 -0700

    Create Outputs.java

---
+++ Outputs.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Line 1 output");//String output with new line insertion
+        System.out.print("Line2 ");
+        System.out.println("Line3 output");//String output without new line insertion
+
+        System.out.println(60);//Integer output
+        System.out.println("String "+9);//String output with integer
+    }
+}
