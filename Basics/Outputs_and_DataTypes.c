c8d033f47028493ddd7c3b0c54112b03bb7c7405
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-08-07 11:22:43 -0700

    Create Outputs_and_DataTypes.c

---
+++ Basics/Outputs_and_DataTypes.c
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Student data
+        String Name = "Jhonny";//string
+        String Profession = "Multi-Profession";
+        char Rating = 'A';//character
+        int ID = 130013;//integer
+        int Exp = 69;
+        float Salary = 1.25f;//float
+        boolean Status = true;//boolean
+
+
+        // Print variables
+        System.out.println("Employee name: " + Name);
+        System.out.println("ID: " + ID);
+        System.out.println("Profession: " + Profession);
+        System.out.println("Rating:" + Rating);
+        System.out.println("Experience: " + Exp);
+        System.out.println("Salary: " + Salary + "Cr");
+        System.out.println("Active status: " + Status);
+    }
+}
