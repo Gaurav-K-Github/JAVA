72a6e947d756c6cf140577c1bed897ccab9458e3
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-20 11:27:07 -0700

    Create Conditionals.java

---
+++ Basics/Control of a program/Conditionals.java
@@ -0,0 +1,22 @@
+package GIT_projects.src;
+
+public class Conditionals {
+    public static void main(String[] args){
+        int i1=2;
+        if (i1!=0){//Simple if condition
+            System.out.println("Non zero number!");
+        };
+
+        int i2=0;
+        if (i2!=0){//If-Else statement
+            System.out.println("Non zero number!");
+        }
+        else{
+            System.out.println("Zero!");
+        };
+
+
+
+
+    }
+}
