660d94a53f6dcdd8829e73ca615da574fed4f36d
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-26 02:16:26 -0700

    Create MethodCalls.java

---
+++ Basics/MethodCalls.java
@@ -0,0 +1,11 @@
+package GIT_projects.src;
+
+public class MethodCalls {
+    public static void main(String[] args){
+        testmethod();
+    }
+
+    public static void testmethod(){
+        System.out.println("Test method working!");
+    }
+}
