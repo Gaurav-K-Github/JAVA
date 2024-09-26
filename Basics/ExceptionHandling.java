f8fa90a791e18ecad58c113962a9bb3a81ffba60
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-26 02:35:48 -0700

    Create ExceptionHandling.java

---
+++ Basics/ExceptionHandling.java
@@ -0,0 +1,29 @@
+package GIT_projects.src;
+
+import java.io.IOException;
+
+public class ExceptionHandling {
+    public static void main(String[] args){
+        //a.Try-Catch & Finally
+        try {
+            int res = 10 / 0;//throws ArithmeticException
+        }catch(ArithmeticException e){
+            System.out.println("Not possible to divide!!");
+
+        }finally{
+            System.out.println("Always got this one executing!");
+        }
+
+        //b.Throw
+        int age=0;
+        if (age==0){
+            throw new IllegalArgumentException("Age 18+");
+        }
+
+    }
+    //c.Throws clause
+    public void riskymethod() throws IOException{
+        //might throw an IO Exception
+    }
+
+}
