95459e71cdfcb9dbbbe0688d93022b41eecc2af0
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-27 10:50:09 -0700

    Create Q2.java

---
+++ 100+ Java PPs/Q2.java
@@ -0,0 +1,14 @@
+//2. How do you declare a variable in Java? Write a program to declare and initialize
+//variables of different types.
+
+public class Q2 {
+    public static void main(String[] args){
+        int variable1;//Declaration
+        variable1=4;//Initialization
+
+        int variable2=69;//Declaration and initialization at same time
+
+        System.out.println(variable1);
+
+    }
+}
