b2940ce874fa70ea7366e8806032e0a91cea5991
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-30 10:17:47 -0700

    Create Q10.java

---
+++ 100+ Java PPs/Q10.java
@@ -0,0 +1,12 @@
+//Q10.WAP to reverse a number
+
+public class Q7 {
+    public static void main(String[] args) {
+        int num=1234556;
+
+        while(num!=0){
+            System.out.println(num%10);
+            num=num/10;
+        }
+    }
+}
