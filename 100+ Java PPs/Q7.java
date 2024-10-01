f37254f1a7205df5760eb02077fcdcce728b0af7
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-30 23:46:27 -0700

    Create Q7.java

---
+++ 100+ Java PPs/Q7.java
@@ -0,0 +1,15 @@
+//7. Write a program to check whether a number is even or odd.
+
+public class Q7{
+    public static void main(String[] args) {
+        int n1=0;
+
+        if(n1==0){
+            System.out.println("Zero found!");
+        }else if (n1%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
