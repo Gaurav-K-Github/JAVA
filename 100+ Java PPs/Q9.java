c1927783953eb90e5847aa608ded4a82ba88850e
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-10-01 00:16:09 -0700

    Create Q9.java

---
+++ 100+ Java PPs/Q9.java
@@ -0,0 +1,19 @@
+//9. Write a program to find the factorial of a number using loops.
+
+import java.util.*;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Enter number:");
+        int n=scan.nextInt();
+        int f=1;
+
+        for (int i=1;i<=n;i++){
+            f=f*i;
+        }
+
+        System.out.println("Factorial of "+n+" = "+f);
+    }
+}
