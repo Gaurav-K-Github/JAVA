27cdf197ba9d6fc603ad283eb847f80705b1abf5
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-11-13 23:54:42 +0530

    Update Conditionals.java

--- Basics/Control of a program/Conditionals.java
+++ Basics/Control of a program/Conditionals.java
@@ -6,10 +6,6 @@ public class Conditionals {
         if (i1!=0){//Simple if condition
             System.out.println("Non zero number!\n");
         };
-
-
-
-
         
         int i2=0;
         if (i2!=0){//If-Else statement
