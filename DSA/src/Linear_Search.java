7545f4d68e037fcec0275bbc719e9444f757ff7b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2025-02-09 23:50:35 +0530

    Update Linear_Search.java

--- DSA/src/Linear_Search.java
+++ DSA/src/Linear_Search.java
@@ -4,8 +4,6 @@ public class Linear_Search {
         int target=8;
         boolean flag=false;
 
-
-
         for (int i=0;i<arr1.length;i++) {
             if (arr1[i] == target) {
                 System.out.println(target + " found @ " + i + " index!");
